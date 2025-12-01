83e2c6a0812341727f6937709391bec585a84141
Umarfarooq J Ron <umarron.10@gmail.com>
2025-12-01 15:56:40 +0530

    Update test_employe.py

--- test_employe.py
+++ test_employe.py
@@ -3,7 +3,7 @@ from employee import employee_details
 def test_employee_details():
     expected_output = (
         "Employee Name: Alice\n"
-        "Employee ID: E101\n"
+        "Employee ID: e101\n"
         "Department: IT\n"
         "Salary: 55000"
     )
